5c94792574afcb7f5b15c84d77af2c5d9f2988d6
tgk22c <tgk22c@naver.com>
2023-10-10 12:58:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('./index.html')
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.get_json()  # JSON 형태의 데이터를 받아옵니다.
+    text = data.get('text')  # 'text' 키에 해당하는 값을 가져옵니다.
+    processed_text = text.upper()  # 문자열 가공 예시 (대문자로 변환)
+    return jsonify({'processed_text': processed_text})  # JSON 형태로 결과 반환
+
+if __name__ == '__main__':
+    app.run(debug=True)
