acb56f794a411529373369c9901edb16b3c72ad9
tgk22c <tgk22c@naver.com>
2023-10-10 20:25:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# Flask Server Code
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -6,11 +5,9 @@ app = Flask(__name__)
 @app.route('/reverse_case', methods=['POST'])
 def reverse_case():
     data = request.get_json()
-    if 'text' in data:
-        reversed_text = data['text'].swapcase()
-        return jsonify({'reversed_text': reversed_text})
-    else:
-        return jsonify({'error': 'No text field provided.'}), 400
+    text = data['text']
+    reversed_text = ''.join(c.lower() if c.isupper() else c.upper() for c in text)
+    return jsonify({'reversed_text': reversed_text})
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
