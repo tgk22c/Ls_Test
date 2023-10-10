82aa975d052816efa3414e57d294091f85600fc6
tgk22c <tgk22c@naver.com>
2023-10-10 13:10:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/process', methods=['POST'])
-def process():
-    data = request.get_json()  # JSON 형태의 데이터를 받아옵니다.
-    text = data.get('text')  # 'text' 키에 해당하는 값을 가져옵니다.
-    processed_text = text.upper()  # 문자열 가공 예시 (대문자로 변환)
-    return jsonify({'processed_text': processed_text})  # JSON 형태로 결과 반환
+@app.route('/reverse_case', methods=['POST'])
+def reverse_case():
+    data = request.get_json()
+    original_text = data['text']
+    reversed_text = original_text.swapcase()
+    return jsonify({'reversed_text': reversed_text})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
