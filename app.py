a63e9a9a310f6682fcb9383840f3bf91e3e92e3c
tgk22c <tgk22c@naver.com>
2023-10-11 13:22:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route('/reverse_case', methods=['POST'])
 def reverse_case():
-    data = request.get_json()
-    text = data['text']
-    reversed_text = ''.join(c.lower() if c.isupper() else c.upper() for c in text)
-    return jsonify({'reversed_text': reversed_text})
+    data = request.get_json()  # JSON 형태의 데이터를 받아옵니다.
+    text = data.get('text')  # 'text' 키로 데이터에서 문자열을 추출합니다.
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    if text:
+        reversed_text = text.swapcase()  # 대소문자를 바꿉니다.
+        return jsonify({'reversed_text': reversed_text})  # 결과값을 JSON 형태로 반환합니다.
+    
+    else:
+        return jsonify({'error': 'No text provided.'}), 400
